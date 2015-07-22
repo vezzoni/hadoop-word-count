d7bfb0a0702fd1d272cbeb59153492dbdd7b5b49
Roberto Vezzoni <roberto.vezzoni@gmail.com>
2015-07-22 14:47:25 -0300

    uploading the code.

---
+++ src/main/java/vezzoni/word/count/main/WordCount.java
@@ -0,0 +1,106 @@
+package vezzoni.word.count.main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+import org.apache.hadoop.fs.Path;
+//import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+//import org.apache.hadoop.util.*;
+
+public class WordCount {
+
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private final Text word = new Text();
+
+        @Override
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            
+            System.out.println("mapping...");
+            
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                output.collect(word, one);
+            }
+        }
+    }
+
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            
+            System.out.println("reducing...");
+            
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        
+        if (args.length < 2) {
+            System.out.println("invalid args");
+            System.exit(-1);
+        }
+        
+        String input = args[0];
+        String output = args[1];
+        
+        File in = new File(input);
+        File out = new File(output);
+        
+        if (!in.isDirectory()) {
+            System.out.println("invalid input directory");
+            System.exit(-1);
+        }
+        if (!out.isDirectory()) {
+            System.out.println("invalid output directory");
+            System.exit(-1);
+        }
+        
+        WordCount.deleteDirectory(out);
+        
+        JobConf conf = new JobConf(WordCount.class);
+        conf.setJobName("wordcount");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(Map.class);
+        conf.setCombinerClass(Reduce.class);
+        conf.setReducerClass(Reduce.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(input));
+        FileOutputFormat.setOutputPath(conf, new Path(output));
+
+        System.out.println("running...");
+        JobClient.runJob(conf);
+    }
+
+    static public boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return (path.delete());
+    }
+}
